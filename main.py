4e2d1aca120d6bb691bb759e22d81a88098672fd
Tapeline <yt.redstone.mail@gmail.com>
2024-12-01 19:33:14 +0500

    Add all features to master

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+from add import add
+from division import div
+from programm_vichitaet import main as sub
+from umnoenie import mul
+
 def main():
     print("Hello, World!")
-
+    cmd = input()
+    {
+        "+": add,
+        "-": sub,
+        "*": mul,
+        "/": div
+    }[cmd]()
 
 if __name__ == '__main__':
     main()
